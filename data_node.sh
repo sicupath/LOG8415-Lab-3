08c5f489157408278a6c317c4b8ae4e52a2cc665
Sofia Ingigerth Canas Urbina <84815840+sicupath@users.noreply.github.com>
2024-01-02 00:25:46 +0000

    Commenting data_node configuration

--- data_node.sh
+++ data_node.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+# Update the package list to ensure we have the latest versions of packages and their dependencies
 sudo apt update
+
+# Install necessary Perl library and ncurses library which are prerequisites for MySQL Cluster
 sudo apt install libclass-methodmaker-perl libncurses5 -y
 
+# Change the working directory to the home directory of the 'ubuntu' user
+cd /home/ubuntu
+
+# Download the MySQL Cluster Data Node package
 wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/mysql-cluster-community-data-node_7.6.6-1ubuntu18.04_amd64.deb
+
+# Install the downloaded MySQL Cluster Data Node package
 sudo dpkg -i mysql-cluster-community-data-node_7.6.6-1ubuntu18.04_amd64.deb
 
-
+# Configure the MySQL Cluster by appending the configuration to the my.cnf file
 echo "
 [mysql_cluster]
-# Options for NDB Cluster processes:
-ndb-connectstring=ip-172-31-17-1.ec2.internal # location of cluster manager
+ndb-connectstring=ip-172-31-22-6.ec2.internal 
 " | tee -a /etc/my.cnf
 
-
+# Create a directory for MySQL data
 sudo mkdir -p /usr/local/mysql/data
 
-
+# Create a new systemd service file for the MySQL NDB Data Node Daemon
 echo "
 [Unit]
 Description=MySQL NDB Data Node Daemon
@@ -33,7 +41,11 @@ Restart=on-failure
 WantedBy=multi-user.target
 " | tee -a /etc/systemd/system/ndbd.service
 
-
+# Reload the systemd daemon to recognize the new service
 sudo systemctl daemon-reload
+
+# Enable the newly created service to start on boot
 sudo systemctl enable ndbd
+
+# Start the MySQL NDB Data Node service immediately
 sudo systemctl start ndbd
